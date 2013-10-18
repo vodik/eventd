39e1c1f5194c3c937db045ddae3e87a221dae3a6
Simon Gomizelj <simongmzlj@gmail.com>
2013-10-17 21:37:13 -0400

    cleanups

--- eventd.c
+++ eventd.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <err.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <linux/input.h>
-
-static volatile sig_atomic_t got_SIGCHLD = 0;
-
-static void child_sig_handler(int sig)
-{
-	/* got_SIGCHLD = 1; */
-}
 
 static void read_event(int fd)
 {
@@ -22,8 +17,8 @@ static void read_event(int fd)
 	if (event.type == 0)
 		return;
 
-	char type[6], code[6];
-	char value[12];
+	char type[6], code[6]; /* u16: 5 digits + null */
+	char value[12];        /* s32: 10 digits + sign + null */
 
 	sprintf(type, "%d", event.type);
 	sprintf(code, "%d", event.code);
@@ -31,6 +26,7 @@ static void read_event(int fd)
 
 	switch (fork()) {
 		case -1:
+			err(1, "fork failed");
 			break;
 		case 0:
 			execl("./eventd.sh", "eventd.sh", type, code, value, NULL);
@@ -45,42 +41,17 @@ static void read_event(int fd)
 
 static void select_events(int fd)
 {
-	sigset_t sigmask, empty_mask;
-	struct sigaction sa;
 	fd_set rd;
 	int nfds, r;
-
-	sigemptyset(&sigmask);
-	sigaddset(&sigmask, SIGCHLD);
-	if (sigprocmask(SIG_BLOCK, &sigmask, NULL) == -1) {
-		perror("sigprocmask");
-		exit(EXIT_FAILURE);
-	}
-
-	sa.sa_flags = 0;
-	sa.sa_handler = child_sig_handler;
-	sigemptyset(&sa.sa_mask);
-	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-		perror("sigaction");
-		exit(EXIT_FAILURE);
-	}
-
-	sigemptyset(&empty_mask);
 
 	while (1) {
 		FD_ZERO(&rd);
 		FD_SET(fd, &rd);
 		nfds = fd + 1;
 
-		r = pselect(nfds, &rd, NULL, NULL, NULL, &empty_mask);
-
-		if (r == -1 && errno != EINTR) {
-			perror("pselect");
-			exit(EXIT_FAILURE);
-		}
-
-		if (got_SIGCHLD)
-			return;
+		r = select(nfds, &rd, NULL, NULL, NULL);
+		if (r == -1 && errno != EINTR)
+			err(1, "select falsed");
 
 		if (FD_ISSET(fd, &rd))
 			read_event(fd);
@@ -92,15 +63,15 @@ int main(int argc, char* argv[])
 	int fd = -1;
 	char name[256] = "Unknown";
 
-	if ((fd = open(argv[1], O_RDONLY)) < 0) {
-		perror("evdev open");
-		exit(EXIT_FAILURE);
-	}
+	if (argc < 2)
+		errx(1, "not enough arguments");
 
-	if (ioctl(fd, EVIOCGNAME(sizeof(name)), name) < 0) {
-		perror("evdev ioctl");
-		exit(EXIT_FAILURE);
-	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+		err(1, "evdev open failed");
+
+	if (ioctl(fd, EVIOCGNAME(sizeof(name)), name) < 0)
+		err(1, "evdev ioctl failed");
 
 	printf("The device on %s says its name is %s\n", argv[1], name);
 	select_events(fd);
